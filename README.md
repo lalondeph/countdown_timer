6b2cbf6ec7ee77aeb837b34b3d40a6c8829e0669
Philip Lalonde <lalondeph@gmail.com>
2023-09-02 08:16:20 -0400

    init commit for hype_date

---
+++ README.md
@@ -0,0 +1,3 @@
+# hype_date
+
+A Flutter project for keeping track of important dates
---
+++ android/app/src/main/kotlin/jade/pug/hype_date/MainActivity.kt
@@ -0,0 +1,6 @@
+package jade.pug.hype_date
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
